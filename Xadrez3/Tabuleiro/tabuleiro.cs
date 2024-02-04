c13fab1269b812c8c682dde7ae96069f036266ea
Douglas Nunes <paulodnunes2016@gmail.com>
2024-02-04 10:29:11 -0300

    Xadrez imcompleto

--- Xadrez3/Tabuleiro/tabuleiro.cs
+++ Xadrez3/Tabuleiro/tabuleiro.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.SymbolStore;
 using Tabuleiro;
 namespace Xadrez3.Tabuleiro
 {
@@ -21,11 +22,36 @@ namespace Xadrez3.Tabuleiro
                 return pecas[linha, coluna];
             }
         }
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool ExistePeca(Posicao pos)
+        {
+            return peca(pos) != null;
+        }
 
         internal void ColocarPeca(Torre torre, Posicao posicao)
         {
             throw new NotImplementedException();
         }
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna <= Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void ValidarPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posiçao invlálida");
+            }
+        }
+      
     }
    
 }
